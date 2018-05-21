4e0383a537f8728a992d69721ba92e6be4afdf4c
GaiusC <39379995+GaiusC@users.noreply.github.com>
2018-05-21 14:00:25 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,74 +1,72 @@
 #include <stdio.h>
-#include "./lib/f_lib.h"
+#include <math.h>
+#define PI (3.14159265358979323846)
+#define g (10)
 
-double V0;
-int angle;
+double v0;
+double time;
+double height;
+double distance;
+double angle;
+double hob;//height_of_building
+
 
 int main()
 {
-    printf("Calculate of The Ballistic Impact\n");
-    printf("    /V0\n");
-    printf("   /\n");
-    printf("  / \n");
-    printf(" / ·angle\n");
-    printf("/————————————\n\n");
+    printf("Calculate-of-The-Ballistic-Impact\n");
+    printf("  / v0\n");
+    printf(" /\n");
+    printf("/___distance\n\n");
 
-
-    printf("Please input the V0\n");
-    scanf("%lf",&V0);
+    printf("Please input the height of building\n");
+    scanf("%lf", &hob);
+    printf("Please input the v0\n");
+    scanf("%lf", &v0);
     printf("Please input the angle\n");
-    scanf("%d",&angle);
+    scanf("%lf", &angle);
 
-    int x,y;
-    printf("What do you want?\n1.Height and distance 2.time 3.both");
-    scanf("%d", &x);
-    if(x == 1 || x == 3)
+    angle = (angle/180)*PI;
+
+height = hob + pow( v0 * sin(angle),2)/ (2*g) ;/*(v0*sinθ)²/2g*/
+/*time = (2*v0*sin(angle))/g;//no building*/
+time = ((2*v0*sin(angle))/g) + (sqrt((2*g*hob)+pow(v0*sin(angle),2))-v0*sin(angle))/g;
+/*distance = pow(v0,2) * sin(2 * angle)/ g;//no building*/
+distance = (((2*v0*sin(angle))/g) + (sqrt((2*g*hob)+pow(v0*sin(angle),2))-v0*sin(angle))/g)*v0*cos(angle);
+
+/*printf("height =%lf \n", height);
+printf("time =%lf \n", time);
+printf("distance =%lf \n", distance);*/
+
+int x,y;
+printf("Do you have the specific time?\n1.Yes  2.NO\n");
+scanf("%d",&x);
+
+if (x==1)
+{
+    printf("When do you want the result?\n");
+    scanf("%lf",&time);
+
+
+    height = hob + (v0 * sin(angle) * time) - (g * pow(time,2))/ 2;
+
+    if (height<=0)
     {
-        printf("Do you need the data stream?\n1.Yes 2.No\n");
-        scanf("%d",&y);
+        printf("The height of the given time is 0.\n");
     }
 
-    switch(x)
+    if (height>0)
     {
-        case    1:
-            switch(y)
-            {
-                case 1:
-                    hei_dis(V0, angle, 1);
-                    break;
-
-                case 2:
-                    hei_dis(V0, angle, 0);
-                    break;
-
-                default:
-                    printf("Wrong choice, please run this program again.");
-            }
-            break;
-
-        case    2:
-            time(V0, angle);
-            break;
-
-        case    3:
-            switch(y)
-            {
-                case 1:
-                    hei_dis(V0, angle, 1);
-                    time(V0, angle);
-                    break;
-
-                case 2:
-                    hei_dis(V0, angle, 0);
-                    time(V0, angle);
-                    break;
-
-                default:
-                    printf("Wrong choice, please run this program again.");
-            }
-
-        default:
-            printf("Wrong choice, please run this program again.");
+        printf("The height of the given time is %f.\n",height);
     }
-    return 0;
+}
+
+if (x==2)
+{
+    printf("height =%lf \n", height);
+    printf("time =%lf \n", time);
+    printf("distance =%lf \n", distance);
+}
+
+if (x!=1,x!=2) return NULL;
+    return r;
 }
